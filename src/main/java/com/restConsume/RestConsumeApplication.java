bfe22c6cab45359d0f7d9690b6ae6ab519ff7ba0
ufuk058 <ufuksahinduran.uk@gmail.com>
2024-12-01 16:27:25 +0000

    initial commit

---
+++ src/main/java/com/restConsume/RestConsumeApplication.java
@@ -0,0 +1,13 @@
+package com.restConsume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestConsumeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestConsumeApplication.class, args);
+	}
+
+}
